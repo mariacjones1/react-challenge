5da533de19af562c85b5a5658fdf541184b463cd
mariacjones1 <mariacjones1@hotmail.co.uk>
2023-09-19 16:31:41 +0000

    Create basic sidebar component

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Sidebar = () => {
+    return (
+        <div>
+            <a href="#">My Photos</a>
+            <a href="#">My Illustrations</a>
+            <a href="#">My Paintings</a>
+        </div>
+    );
+}
+
+export default Sidebar
